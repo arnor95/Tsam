a8b3fdc12e76c0a0bf721acfc110ebae81df99f1
Arnor Rafn Gislason <arnorrg15@ru.is>
2017-09-07 23:05:38 +0000

    some shiet

--- src/tftpd.c
+++ src/tftpd.c
@@ -1,6 +1,97 @@
-/* your code goes here. */
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <dirent.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <ctype.h>
+#include <stdio.h>
+
+int getPort(char *argv[]);
+char* getDir(char *argv[]);
+struct dirent* readFile(char* dir_path, char* file_name);
+//void setUpUdp(port);
 
 int main(int argc, char *argv[])
 {
+	if (argc > 2)
+	{
+		int port = getPort(argv);
+		char* dir = getDir(argv);
+		printf(dir);
+		printf(" ""%d", port);
+		char* file_name = "example_data1";
+		readFile(dir, file_name);
+	}
+
+	else{
+		printf("Must have 2 parameters in main function!");
+	}
+	//setUpUdp(port);
 	return 0;
 }
+
+int getPort(char *argv[])
+{
+	return atoi(argv[1]);
+}
+
+char* getDir(char *argv[])
+{
+	return argv[2];
+}
+
+struct dirent* readFile(char* dir_path, char* file_name)
+{
+	DIR * dir = opendir(dir_path);
+	struct dirent* file;
+
+	while (file = readdir(dir) != NULL)
+	{
+		if (strcmp(file, file_name) == 0)
+		{
+			return file;
+			printf("Found file!");
+		}
+	}
+	return NULL;
+}
+
+/*void setUpUdp(int port)
+{
+    int sockfd;
+    struct sockaddr_in server, client;
+    char message[512];
+
+    // Create and bind a UDP socket.
+    sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    memset(&server, 0, sizeof(server));
+    server.sin_family = AF_INET;
+
+    // Network functions need arguments in network byte order instead
+    // of host byte order. The macros htonl, htons convert the values.
+    server.sin_addr.s_addr = htonl(INADDR_ANY);
+    server.sin_port = htons(port);
+    bind(sockfd, (struct sockaddr *) &server, (socklen_t) sizeof(server));
+
+    for (;;) {
+        // Receive up to one byte less than declared, because it will
+        // be NUL-terminated later.
+        socklen_t len = (socklen_t) sizeof(client);
+        ssize_t n = recvfrom(sockfd, message, sizeof(message) - 1,
+                             0, (struct sockaddr *) &client, &len);
+
+        message[n] = '\0';
+        fprintf(stdout, "Received:\n%s\n", message);
+        fflush(stdout);
+
+        // convert message to upper case.
+        for (int i = 0; i < n; ++i) {
+            message[i] = toupper(message[i]);
+        }
+
+        sendto(sockfd, message, (size_t) n, 0,
+               (struct sockaddr *) &client, len);
+	}
+	*/
+//}
